af86f961e03ce6e1be7e3ebcebd459a30b061e5e
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-26 11:41:01 -0600

    Initial Commit

---
+++ SwiftUI_ToolBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_ToolBox
+//
+//  Created by J. DeWeese on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_ToolBox/SwiftUI_ToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToolBoxApp.swift
+//  SwiftUI_ToolBox
+//
+//  Created by J. DeWeese on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
